272bc7bed9ad04ced11d89354f863995164e55e8
joshua530 <joshuaomari5@gmail.com>
2022-03-15 11:59:45 +0300

    Remove ERR_HTTP_HEADERS_SENT error

--- middleware/auth-middleware.js
+++ middleware/auth-middleware.js
@@ -3,24 +3,29 @@ const { secretKey } = require('../config/secret-key');
 
 const authenticate = (req, res, next) => {
   const token = req.body.token;
-  const protectedRoutes = [/\/profile\/.+\/update/];
+  const protectedRoutes = ['/api/v1/profile/.+/update'];
   const uri = req.path;
-  for (let path in protectedRoutes) {
+  for (let path of protectedRoutes) {
     if (uri.match(path)) {
-      if (!token)
+      if (!token) {
         res.status(401).json({ error: 'token required for authentication' });
+        return;
+      }
       try {
         const decoded = jwt.verify(token, secretKey);
         const userSlug = decoded['user_slug'];
         const requestSlug = req.params.slug;
-        if (userSlug !== requestSlug)
+        if (userSlug !== requestSlug) {
           res.status(403).json({ error: 'invalid token' });
+          return;
+        }
       } catch (err) {
         res.status(401).json({ error: 'invalid token' });
+        return;
       }
     }
   }
-  return next();
+  next();
 };
 
 module.exports = authenticate;
