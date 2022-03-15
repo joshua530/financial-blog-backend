9904dc0d4bef9b2c270fc60e3f324fd465e40d7d
joshua530 <joshuaomari5@gmail.com>
2022-03-15 12:48:32 +0300

    Add profile editing functionality

--- controllers/profile-controller.js
+++ controllers/profile-controller.js
@@ -1,6 +1,11 @@
 const User = require('../models/user-model');
 const asyncHandler = require('express-async-handler');
-const { cleanUser } = require('../utils/models');
+const {
+  cleanUser,
+  ensureEmailIsUnique,
+  ensureUsernameIsUnique
+} = require('../utils/models');
+const jwt = require('jsonwebtoken');
 
 /**
  * @does fetches user profile
@@ -31,45 +36,50 @@ const updateProfile = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error('User slug cannot be empty');
   }
-  const user = User.findOne({ slug });
+  let user = await User.findOne({ slug });
   if (!user) {
     res.status(400);
     throw new Error('User with that slug does not exist');
   }
 
-  //PUT
-  if (req.method === 'PUT') {
-    // TODO get authentication details
-    // if user is not authenticated, return error with 401 response
-    const username =
-      req.body.username === null ? req.body.username : req.body.username.trim();
-    const email =
-      req.body.email === null ? req.body.email : req.body.email.trim();
-    if (!username) {
-      res.status(400);
-      throw new Error('Username cannot be empty');
+  const username =
+    req.body.username === null ? req.body.username : req.body.username.trim();
+  const email =
+    req.body.email === null ? req.body.email : req.body.email.trim();
+  if (!username) {
+    res.status(400);
+    throw new Error('Username cannot be empty');
+  }
+  if (!email) {
+    res.status(400);
+    throw new Error('Password cannot be empty');
+  }
+  if (!email.match(/[a-zA-Z][a-zA-Z0-9]+@[a-zA-Z0-9]{3,}\.[a-zA-Z]{2,}/)) {
+    res.status(400);
+    throw new Error('invalid email provided');
+  }
+  console.log(user);
+
+  if (email !== user.email) {
+    await ensureEmailIsUnique(req, res, email);
+  }
+  if (username !== user.username) {
+    await ensureUsernameIsUnique(req, res, username);
+  }
+  let updatedUser = await User.updateOne(
+    { slug },
+    {
+      username,
+      email
     }
-    if (!email) {
-      res.status(400);
-      throw new Error('Password cannot be empty');
-    }
-    if (!email.match(/[a-zA-Z][a-zA-Z0-9]+@[a-zA-Z0-9]{3,}\.[a-zA-Z]{2,}/)) {
-      res.status(400);
-      throw new Error('invalid email provided');
-    }
-    const updatedUser = await User.updateOne(
-      { slug },
-      {
-        $set: {
-          username,
-          email
-        }
-      }
-    );
-    res.status(200).json(updatedUser);
+  );
+  if (!updatedUser) {
+    res.status(400).json({ error: 'could not update user' });
   } else {
-    res.status(200).json({ username: user.username, email: user.email });
+    console.log(updatedUser);
+    const updated = { username, email };
+    res.status(200).json(updated);
   }
 });
 
-module.exports = { viewProfile, editProfile: updateProfile };
+module.exports = { viewProfile, updateProfile };
--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const { viewProfile } = require('../controllers/profile-controller');
+const {
+  viewProfile,
+  updateProfile
+} = require('../controllers/profile-controller');
 const router = express.Router();
 
 router.route('/:slug').get(viewProfile);
+router.route('/:slug/update').put(updateProfile);
 
 module.exports = router;
