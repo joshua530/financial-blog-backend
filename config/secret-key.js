aa1ce974cbe91914152499f9dba503994edb2331
joshua530 <joshuaomari5@gmail.com>
2022-03-15 10:26:21 +0300

    Move secret key to .env

--- config/secret-key.js
+++ config/secret-key.js
@@ -1,7 +1,7 @@
 // generated using
 // const salt = crypto.randomBytes(32).toString('base64');
 
-const secretKey = 'DrimkNARbAqQlnTHTqZbVvS0yrwsqbp08r/8BfH7LpY=';
+const secretKey = process.env.SECRET_KEY;
 const numBytes = 32;
 const numIterations = 80000;
 const keyLength = 64;
