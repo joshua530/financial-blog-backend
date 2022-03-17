2fd3fab035bae6f03fe9b76bf36e91d7e52c3d01
joshua530 <joshuaomari5@gmail.com>
2022-03-17 23:27:02 +0300

    Add access-control-allow-methods header

--- middleware/headers-middleware.js
+++ middleware/headers-middleware.js
@@ -1,6 +1,7 @@
 const headerSetter = (req, res, next) => {
   res.setHeader('Access-control-allow-origin', '*');
   res.setHeader('Access-control-allow-headers', 'content-type');
+  res.setHeader('Access-control-allow-methods', 'PUT,POST,GET');
   next();
 };
 
